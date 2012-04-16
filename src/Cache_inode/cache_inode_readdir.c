828d44b5d7c9e333607fcca0286409f6d94bc31d
Adam C. Emerson <aemerson@linuxbox.com>
2012-04-16 12:25:23 -0400

    Fix strict compile failure

--- src/Cache_inode/cache_inode_readdir.c
+++ src/Cache_inode/cache_inode_readdir.c
@@ -323,11 +323,11 @@ cache_entry_t *cache_inode_operate_cached_dirent(cache_entry_t * pentry_parent,
 {
   cache_entry_t *pentry = NULL;
   cache_inode_dir_entry_t dirent_key[1], *dirent, *dirent2, *dirent3;
-  LRU_List_state_t vstate;
+  cache_inode_entry_valid_state_t vstate;
   int code = 0;
 
-  /* Directory mutation generally invalidates outstanding 
-   * readdirs, hence any cached cookies, so in these cases we 
+  /* Directory mutation generally invalidates outstanding
+   * readdirs, hence any cached cookies, so in these cases we
    * clear the cookie avl */
 
   /* Set the return default to CACHE_INODE_SUCCESS */
