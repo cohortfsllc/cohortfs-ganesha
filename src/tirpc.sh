98a02a922de172a16cba0306ca878232be18e556
Matt Benjamin <matt@linuxbox.com>
2012-05-02 12:59:39 -0400

    Move tirpc branch point to duplex-3.

--- src/tirpc.sh
+++ src/tirpc.sh
@@ -3,7 +3,7 @@
 OPWD=`pwd`
 
 TIRPC_REPO='https://github.com/mattbenjamin/libtirpc-lbx.git'
-TIRPC_BRANCH='duplex-2'
+TIRPC_BRANCH='duplex-3'
 
 cd ../contrib
 git clone -b ${TIRPC_BRANCH} ${TIRPC_REPO} libtirpc
