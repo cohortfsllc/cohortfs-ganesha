ee54807304aa924432c233108ddbd9c3044d7955
Matt Benjamin <matt@linuxbox.com>
2013-05-01 13:20:49 -0700

    ds_cache 3.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

---
+++ src/FSAL/FSAL_CEPH/ds_cache.c
@@ -0,0 +1,117 @@
+/*
+ * Copyright © 2013, CohortFS, LLC.
+ * Author: Matt Benjamin <matt@linuxbox.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/**
+ * @file   ds_cache.h
+ * @author Matt Benjamin <matt@linuxbox.com>
+ * @date   Wed May  1 08:29:47 PDT 2013
+ *
+ * @brief Ceph FSAL DS reservation cache (decls)
+ *
+ * Ceph FSAL DS reservation cache (decls).
+ */
+
+#include "config.h"
+
+#include "ds_cache.h"
+#include "fsal.h"
+#include "fsal_api.h"
+#include "FSAL/fsal_commonlib.h"
+#include "internal.h"
+
+struct ds_rsv_cache ds_cache;
+
+/**
+ * Expanded reservation comparaison function.
+ *
+ * Orders by inode, then rsv.id.
+ */
+static int
+rsv_cmpf(const struct avltree_node *lhs,
+	 const struct avltree_node *rhs)
+{
+	struct ds_rsv *lk, *rk;
+
+	lk = avltree_container_of(lhs, struct ds_rsv, node_k);
+	rk = avltree_container_of(rhs, struct ds_rsv, node_k);
+
+	if (lk->ino < rk->ino)
+		return (-1);
+
+	if (lk->ino == rk->ino) {
+		if (lk->rsv.id < rk->rsv.id)
+			return (-1);
+
+		if (lk->rsv.id == rk->rsv.id)
+			return (0);
+	}
+
+	return (1);
+}
+
+
+/**
+ * @brief Initialize a queue.
+ *
+ * Initialize a queue.
+ */
+static inline void
+lru_init_queue(struct rsv_q_lane *qlane)
+{
+	pthread_mutex_init(&qlane->mtx, NULL);
+	init_glist(&qlane->q);
+	qlane->size = 0;
+}
+
+/**
+ *  Package init method
+ */
+void ds_cache_pkginit(void)
+{
+	int ix;
+	for (ix = 0; ix < RSV_N_Q_LANES; ++ix) {
+		struct rsv_q_lane *qlane = &ds_cache.lru[ix];
+		lru_init_queue(qlane);
+	}
+
+	ds_cache.max_entries = 2048; /* TODO:  conf */
+	ds_cache.n_entries = 0;
+	ds_cache.rcache.cachesz = 8192; /* 56K slot table */
+
+	(void) avlx_init(&ds_cache.rcache, rsv_cmpf, 7, AVL_X_FLAG_CACHE_RT);
+}
+
+/**
+ * @brief Get the appropriate lane for a reservation object
+ *
+ * This function gets the LRU lane by taking the modulus of the
+ * supplied pointer.
+ *
+ * @param[in] entry  A pointer to expanded lru
+ *
+ * @return The LRU lane in which that object should be stored.
+ */
+static inline uint32_t
+lru_lane_of_rsv(const struct ds_rsv *rsv)
+{
+	return (uint32_t) (((uintptr_t) rsv) % RSV_N_Q_LANES);
+}
---
+++ src/FSAL/FSAL_CEPH/ds_cache.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright © 2013, CohortFS, LLC.
+ * Author: Matt Benjamin <matt@linuxbox.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/**
+ * @file   ds_cache.h
+ * @author Matt Benjamin <matt@linuxbox.com>
+ * @date   Wed May  1 08:29:47 PDT 2013
+ *
+ * @brief Ceph FSAL DS reservation cache (decls)
+ *
+ * Ceph FSAL DS reservation cache (decls).
+ */
+
+#ifndef FSAL_CEPH_DS_CACHE_H
+#define FSAL_CEPH_DS_CACHE_H
+
+#include <cephfs/libcephfs.h>
+#include "avl_x.h"
+#include "nlm_list.h"
+#include "abstract_mem.h"
+
+/**
+ * Expanded reservation (local cache)
+ */
+
+#define DS_RSV_FLAG_NONE   0x0000
+#define DS_RSV_FLAG_FENCED 0x0001
+
+struct ds_rsv {
+	uint64_t hk;
+	uint32_t flags;
+	int32_t refcnt;
+	struct avltree_node node_k; /*< AVL node in tree */
+	struct ceph_reservation rsv;
+	uint64_t ino;
+};
+
+/**
+ * Reservation support.
+ */
+
+/* Cache-line padding macro from MCAS */
+#ifndef CACHE_LINE_SIZE
+#define CACHE_LINE_SIZE 64 /* XXX arch-specific define */
+#endif
+#define CACHE_PAD(_n) char __pad ## _n [CACHE_LINE_SIZE]
+#define ALIGNED_ALLOC(_s)					\
+     ((void *)(((unsigned long)malloc((_s)+CACHE_LINE_SIZE*2) + \
+		CACHE_LINE_SIZE - 1) & ~(CACHE_LINE_SIZE-1)))
+
+#define RSV_N_Q_LANES 7
+
+struct rsv_q_lane
+{
+	pthread_mutex_t mtx;
+	struct glist_head q; /* LRU is at HEAD, MRU at tail */
+	uint32_t size;
+	CACHE_PAD(0);
+};
+
+struct ds_rsv_cache
+{
+	uint32_t max_entries; /* TODO:  move to config file */
+	uint32_t n_entries;
+
+	struct avl_x rcache;
+	struct rsv_q_lane lru[RSV_N_Q_LANES];
+};
+
+extern struct ds_rsv_cache ds_cache;
+
+void ds_cache_pkginit();
+
+
+#endif /* FSAL_CEPH_DS_CACHE_H */
