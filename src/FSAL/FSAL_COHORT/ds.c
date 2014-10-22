e285f875670ebe0b3b7cb1afa24f7ea936b78bbf
William Allen Simpson <bill@cohortfs.com>
2014-10-22 13:53:10 -0400

    Integrate FSAL_COHORT
    Signed-off-by: William Allen Simpson <bill@cohortfs.com>
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

---
+++ src/FSAL/FSAL_COHORT/ds.c
@@ -0,0 +1,214 @@
+/*
+ * Copyright © 2012-2014, CohortFS, LLC.
+ * Author: Adam C. Emerson <aemerson@linuxbox.com>
+ *
+ * contributeur : William Allen Simpson <bill@cohortfs.com>
+ *		  Marcus Watts <mdw@cohortfs.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/**
+ * @file   ds.c
+ * @author Adam C. Emerson <aemerson@linuxbox.com>
+ * @author William Allen Simpson <bill@cohortfs.com>
+ * @author Marcus Watts <mdw@cohortfs.com>
+ * @date Wed Oct 22 13:24:33 2014
+ *
+ * @brief pNFS DS operations for Cohort
+ *
+ * This file implements the read, write, commit, and dispose
+ * operations for Cohort data-server handles.  The functionality to
+ * create a data server handle is in the export.c file, as it is part
+ * of the export object's interface.
+ */
+
+#include "config.h"
+
+#include <cephfs/libcephfs.h>
+#include <fcntl.h>
+#include "fsal.h"
+#include "fsal_api.h"
+#include "FSAL/fsal_commonlib.h"
+#include "fsal_up.h"
+#include "internal.h"
+#include "pnfs_utils.h"
+
+/**
+ * @brief Release a DS object
+ *
+ * @param[in] obj_pub The object to release
+ *
+ * @return NFS Status codes.
+ */
+
+static void release(struct fsal_ds_handle *const ds_pub)
+{
+	/* The private 'full' DS handle */
+	struct cohort_ds *ds = container_of(ds_pub, struct cohort_ds, ds);
+	fsal_ds_handle_uninit(&ds->ds);
+	gsh_free(ds);
+}
+
+/**
+ * @brief Read from a data-server handle.
+ *
+ * NFSv4.1 data server handles are disjount from normal
+ * filehandles (in Ganesha, there is a ds_flag in the filehandle_v4_t
+ * structure) and do not get loaded into cache_inode or processed the
+ * normal way.
+ *
+ * @param[in]  ds_pub           FSAL DS handle
+ * @param[in]  req_ctx          Credentials
+ * @param[in]  stateid          The stateid supplied with the READ operation,
+ *                              for validation
+ * @param[in]  offset           The offset at which to read
+ * @param[in]  requested_length Length of read requested (and size of buffer)
+ * @param[out] buffer           The buffer to which to store read data
+ * @param[out] supplied_length  Length of data read
+ * @param[out] eof              True on end of file
+ *
+ * @return An NFSv4.1 status code.
+ */
+static nfsstat4 ds_read(struct fsal_ds_handle *const ds_pub,
+			struct req_op_context *const req_ctx,
+			const stateid4 *stateid, const offset4 offset,
+			const count4 requested_length, void *const buffer,
+			count4 * const supplied_length,
+			bool * const end_of_file)
+{
+	/* The private 'full' DS handle */
+	struct cohort_ds *ds = container_of(ds_pub, struct cohort_ds, ds);
+	/* The amount actually read */
+	int amount_read =
+	    libosd_read(CohortFSM.osd, ds->object_key, ds->volume, offset,
+			requested_length, (char *)buffer,
+			LIBOSD_READ_FLAGS_NONE, NULL, NULL);
+	if (amount_read < 0)
+		return posix2nfs4_error(-amount_read);
+
+	*supplied_length = amount_read;
+
+	*end_of_file = false;
+
+	return NFS4_OK;
+}
+
+/**
+ *
+ * @brief Write to a data-server handle.
+ *
+ * This performs a DS write not going through the data server unless
+ * FILE_SYNC4 is specified, in which case it connects the filehandle
+ * and performs an MDS write.
+ *
+ * @param[in]  ds_pub           FSAL DS handle
+ * @param[in]  req_ctx          Credentials
+ * @param[in]  stateid          The stateid supplied with the READ operation,
+ *                              for validation
+ * @param[in]  offset           The offset at which to read
+ * @param[in]  write_length     Length of write requested (and size of buffer)
+ * @param[out] buffer           The buffer to which to store read data
+ * @param[in]  stability wanted Stability of write
+ * @param[out] written_length   Length of data written
+ * @param[out] writeverf        Write verifier
+ * @param[out] stability_got    Stability used for write (must be as
+ *                              or more stable than request)
+ *
+ * @return An NFSv4.1 status code.
+ */
+static nfsstat4 ds_write(struct fsal_ds_handle *const ds_pub,
+			 struct req_op_context *const req_ctx,
+			 const stateid4 *stateid, const offset4 offset,
+			 const count4 write_length, const void *buffer,
+			 const stable_how4 stability_wanted,
+			 count4 * const written_length,
+			 verifier4 * const writeverf,
+			 stable_how4 * const stability_got)
+{
+	/* The private 'full' DS handle */
+	struct cohort_ds *ds = container_of(ds_pub, struct cohort_ds, ds);
+	/* The amount actually written */
+	int32_t amount_written =
+	    libosd_write(CohortFSM.osd, ds->object_key, ds->volume, offset,
+			 write_length, (char *)buffer,
+			 (stability_wanted >= DATA_SYNC4)
+			 ? LIBOSD_WRITE_CB_STABLE
+			 : LIBOSD_WRITE_CB_UNSTABLE,
+			 NULL, NULL);
+	if (amount_written < 0)
+		return posix2nfs4_error(-amount_written);
+
+	*written_length = amount_written;
+	*stability_got = stability_wanted;
+
+	memset(*writeverf, 0, NFS4_VERIFIER_SIZE);
+	return NFS4_OK;
+}
+
+/**
+ * @brief Commit a byte range to a DS handle.
+ *
+ * NFSv4.1 data server filehandles are disjount from normal
+ * filehandles (in Ganesha, there is a ds_flag in the filehandle_v4_t
+ * structure) and do not get loaded into cache_inode or processed the
+ * normal way.
+ *
+ * @param[in]  ds_pub    FSAL DS handle
+ * @param[in]  req_ctx   Credentials
+ * @param[in]  offset    Start of commit window
+ * @param[in]  count     Length of commit window
+ * @param[out] writeverf Write verifier
+ *
+ * @return An NFSv4.1 status code.
+ */
+static nfsstat4 ds_commit(struct fsal_ds_handle *const ds_pub,
+			  struct req_op_context *const req_ctx,
+			  const offset4 offset, const count4 count,
+			  verifier4 * const writeverf)
+{
+#ifdef COMMIT_FIX
+	/* The private 'full' export */
+	struct cohort_export *export =
+	    container_of(req_ctx->fsal_export, struct cohort_export, export);
+	/* The private 'full' DS handle */
+	struct cohort_ds *ds = container_of(ds_pub, struct cohort_ds, ds);
+	/* Error return from Cohort */
+	int rc = 0;
+
+	/* Find out what stripe we're writing to and where within the
+	   stripe. */
+
+	rc = ceph_ll_commit_blocks(export->cmount, ds->wire.wire.vi, offset,
+				   (count == 0) ? UINT64_MAX : count);
+	if (rc < 0)
+		return posix2nfs4_error(rc);
+
+#endif				/* COMMIT_FIX */
+
+	memset(*writeverf, 0, NFS4_VERIFIER_SIZE);
+	return NFS4_OK;
+}
+
+void ds_ops_init(struct fsal_ds_ops *ops)
+{
+	ops->release = release;
+	ops->read = ds_read;
+	ops->write = ds_write;
+	ops->commit = ds_commit;
+};
---
+++ src/FSAL/FSAL_COHORT/main.c
@@ -0,0 +1,333 @@
+/*
+ * Copyright © 2012-2014, CohortFS, LLC.
+ * Author: Adam C. Emerson <aemerson@linuxbox.com>
+ *
+ * contributeur : William Allen Simpson <bill@cohortfs.com>
+ *		  Marcus Watts <mdw@cohortfs.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/**
+ * @file FSAL_COHORT/main.c
+ * @author Adam C. Emerson <aemerson@linuxbox.com>
+ * @author William Allen Simpson <bill@cohortfs.com>
+ * @author Marcus Watts <mdw@cohortfs.com>
+ * @date Wed Oct 22 13:24:33 2014
+ *
+ * @brief Implementation of FSAL module founctions for Cohort
+ *
+ * This file implements the module functions for the Cohort FSAL, for
+ * initialization, teardown, configuration, and creation of exports.
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+#include "fsal.h"
+#include "fsal_types.h"
+#include "FSAL/fsal_init.h"
+#include "FSAL/fsal_commonlib.h"
+#include "fsal_api.h"
+#include "internal.h"
+#include "abstract_mem.h"
+#include "nfs_exports.h"
+#include "export_mgr.h"
+
+/**
+ * Cohort global module object.
+ */
+struct cohort_fsal_module CohortFSM;
+
+/**
+ * The name of this module.
+ */
+static const char *module_name = "Cohort";
+
+static struct libosd_init_args CohortOSD = { 0, NULL, NULL, NULL, NULL };
+
+static struct config_item cohort_items[] = {
+	CONF_ITEM_PATH("Configuration", 0, MAXPATHLEN, "",
+		       cohort_fsal_module, where),
+	CONFIG_EOL
+};
+
+static struct config_block cohort_block = {
+	.dbus_interface_name = "org.ganesha.nfsd.config.fsal.cohort",
+	.blk_desc.name = "Cohort",
+	.blk_desc.type = CONFIG_BLOCK,
+	.blk_desc.u.blk.init = noop_conf_init,
+	.blk_desc.u.blk.params = cohort_items,
+	.blk_desc.u.blk.commit = noop_conf_commit
+};
+
+/* init_config
+ * must be called with a reference taken (via lookup_fsal)
+ */
+
+static fsal_status_t init_config(struct fsal_module *module_in,
+				 config_file_t config_struct)
+{
+//	struct cohort_fsal_module *myself =
+//	    container_of(module_in, struct cohort_fsal_module, fsal);
+	struct config_error_type err_type;
+
+	LogDebug(COMPONENT_FSAL,
+		 "Cohort module setup.");
+
+	(void) load_config_from_parse(config_struct,
+				      &cohort_block,
+				      &CohortFSM,
+				      true,
+				      &err_type);
+	if (!config_error_is_harmless(&err_type))
+		return fsalstat(ERR_FSAL_INVAL, 0);
+
+	/* leave default file NULL */
+	if (CohortFSM.where && strlen(CohortFSM.where) > 0)
+		CohortOSD.config = CohortFSM.where;
+
+	CohortFSM.osd = libosd_init(&CohortOSD);
+	if (CohortFSM.osd == NULL) {
+		LogCrit(COMPONENT_FSAL,
+			"Unable to allocate osd");
+		return fsalstat(ERR_FSAL_NOMEM, 0);
+	}
+	return fsalstat(ERR_FSAL_NO_ERROR, 0);
+}
+
+/**
+ * @brief Create a new export under this FSAL
+ *
+ * This function creates a new export object for the Cohort FSAL.
+ *
+ * @todo ACE: We do not handle re-exports of the same cluster in a
+ * sane way.  Currently we create multiple handles and cache objects
+ * pointing to the same one.  This is not necessarily wrong, but it is
+ * inefficient.  It may also not be something we expect to use enough
+ * to care about.
+ *
+ * @param[in]     module_in  The supplied module handle
+ *
+ * @return FSAL status.
+ */
+
+static fsal_status_t create_export(struct fsal_module *module_in,
+				   void *parse_node,
+				   const struct fsal_up_vector *up_ops)
+{
+	/* The status code to return */
+	fsal_status_t status = { ERR_FSAL_NO_ERROR, 0 };
+	/* A fake argument list for Cohort */
+	const char *argv[] = { "FSAL_COHORT", op_ctx->export->fullpath };
+	/* The internal export object */
+	struct cohort_export *export =
+				gsh_calloc(1, sizeof(struct cohort_export));
+	/* The 'private' root handle */
+	struct cohort_handle *handle = NULL;
+	/* Root inode */
+	struct Inode *i = NULL;
+	/* Root vinode */
+	vinodeno_t root;
+	/* Stat for root */
+	struct stat st;
+	/* Return code */
+	int rc;
+	/* Return code from Cohort calls */
+	int cohort_status;
+	/* True if we have called fsal_export_init */
+	bool initialized = false;
+
+	if (export == NULL) {
+		status.major = ERR_FSAL_NOMEM;
+		LogCrit(COMPONENT_FSAL,
+			"Unable to allocate export object for %s.",
+			op_ctx->export->fullpath);
+		goto error;
+	}
+
+	if (fsal_export_init(&export->export) != 0) {
+		status.major = ERR_FSAL_NOMEM;
+		LogCrit(COMPONENT_FSAL,
+			"Unable to allocate export ops vectors for %s.",
+			op_ctx->export->fullpath);
+		goto error;
+	}
+	export_ops_init(export->export.ops);
+	handle_ops_init(export->export.obj_ops);
+	ds_ops_init(export->export.ds_ops);
+	export->export.up_ops = up_ops;
+
+	initialized = true;
+
+	/* allocates ceph_mount_info */
+	cohort_status = ceph_create(&export->cmount, NULL);
+	if (cohort_status != 0) {
+		status.major = ERR_FSAL_SERVERFAULT;
+		LogCrit(COMPONENT_FSAL,
+			"Unable to create Cohort handle for %s.",
+			op_ctx->export->fullpath);
+		goto error;
+	}
+
+	cohort_status = ceph_conf_read_file(export->cmount, NULL);
+	if (cohort_status != 0) {
+		status.major = ERR_FSAL_SERVERFAULT;
+		LogCrit(COMPONENT_FSAL,
+			"Unable to read Cohort configuration for %s.",
+			op_ctx->export->fullpath);
+		goto error;
+	}
+
+	cohort_status = ceph_conf_parse_argv(export->cmount, 2, argv);
+	if (cohort_status != 0) {
+		status.major = ERR_FSAL_SERVERFAULT;
+		LogCrit(COMPONENT_FSAL,
+			"Unable to parse Cohort configuration for %s.",
+			op_ctx->export->fullpath);
+		goto error;
+	}
+
+	cohort_status = ceph_mount(export->cmount, NULL);
+	if (cohort_status != 0) {
+		status.major = ERR_FSAL_SERVERFAULT;
+		LogCrit(COMPONENT_FSAL,
+			"Unable to mount Cohort cluster for %s.",
+			op_ctx->export->fullpath);
+		goto error;
+	}
+
+	if (fsal_attach_export(module_in, &export->export.exports) != 0) {
+		status.major = ERR_FSAL_SERVERFAULT;
+		LogCrit(COMPONENT_FSAL,
+			"Unable to attach export for %s.",
+			op_ctx->export->fullpath);
+		goto error;
+	}
+
+	export->export.fsal = module_in;
+
+	LogDebug(COMPONENT_FSAL,
+		 "Cohort module export %s.",
+		 op_ctx->export->fullpath);
+
+	root.ino.val = CEPH_INO_ROOT;
+// XXX	root.snapid.val = CEPH_NOSNAP;
+	i = ceph_ll_get_inode(export->cmount, root);
+	if (!i) {
+		status.major = ERR_FSAL_SERVERFAULT;
+		goto error;
+	}
+
+	rc = ceph_ll_getattr(export->cmount, i, &st, 0, 0);
+	if (rc < 0) {
+		status = cohort2fsal_error(rc);
+		goto error;
+	}
+
+	rc = construct_handle(&st, i, export, &handle);
+	if (rc < 0) {
+		status = cohort2fsal_error(rc);
+		goto error;
+	}
+
+	export->root = handle;
+	op_ctx->fsal_export = &export->export;
+	return status;
+
+ error:
+	if (i)
+		ceph_ll_put(export->cmount, i);
+
+	if (export) {
+		if (export->cmount)
+			ceph_shutdown(export->cmount);
+		gsh_free(export);
+	}
+
+	if (initialized)
+		initialized = false;
+
+	return status;
+}
+
+/**
+ * @brief Initialize and register the FSAL
+ *
+ * This function initializes the FSAL module handle, being called
+ * before any configuration or even mounting of a Cohort cluster.  It
+ * exists solely to produce a properly constructed FSAL module
+ * handle.
+ */
+
+MODULE_INIT void cohort_initiate(void)
+{
+	struct fsal_module *myself = &CohortFSM.fsal;
+
+	LogDebug(COMPONENT_FSAL,
+		 "Cohort module registering.");
+
+	/* register_fsal seems to expect zeroed memory. */
+	memset(myself, 0, sizeof(*myself));
+
+	if (register_fsal(myself, module_name, FSAL_MAJOR_VERSION,
+			  FSAL_MINOR_VERSION, FSAL_ID_COHORT) != 0) {
+		/* The register_fsal function prints its own log
+		   message if it fails */
+		LogCrit(COMPONENT_FSAL,
+			"Cohort module failed to register.");
+	}
+
+	/* Set up module operations */
+	myself->ops->create_export = create_export;
+	myself->ops->init_config = init_config;
+}
+
+/**
+ * @brief Release FSAL resources
+ *
+ * This function unregisters the FSAL and frees its module handle.
+ * The Cohort FSAL has no other resources to release on the per-FSAL
+ * level.
+ */
+
+MODULE_FINI void cohort_finish(void)
+{
+	LogDebug(COMPONENT_FSAL,
+		 "Cohort module finishing.");
+
+	if (unregister_fsal(&CohortFSM.fsal) != 0) {
+		LogCrit(COMPONENT_FSAL,
+			"Unable to unload Cohort FSAL.  Dying with extreme "
+			"prejudice.");
+		abort();
+	}
+	if (CohortFSM.osd == NULL) {
+		LogDebug(COMPONENT_FSAL,
+			 "Cohort module has no osd object.");
+		goto cleanup;
+	}
+	libosd_shutdown(CohortFSM.osd);
+	libosd_join(CohortFSM.osd);
+	libosd_cleanup(CohortFSM.osd);
+	CohortFSM.osd = NULL;
+
+cleanup:
+	gsh_free(CohortFSM.where);
+	CohortFSM.where = NULL;
+	CohortOSD.config = NULL;
+}
