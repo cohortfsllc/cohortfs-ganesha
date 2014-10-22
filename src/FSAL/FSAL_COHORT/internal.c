6df436626869d9a50d6d87d2ae8ba0d6b7175c2a
William Allen Simpson <bill@cohortfs.com>
2014-10-22 13:53:10 -0400

    Integrate FSAL_COHORT
    Signed-off-by: William Allen Simpson <bill@cohortfs.com>

---
+++ src/FSAL/FSAL_COHORT/internal.c
@@ -0,0 +1,184 @@
+/*
+ * Copyright © 2012-2014, CohortFS, LLC.
+ * Author: Adam C. Emerson <aemerson@linuxbox.com>
+ *
+ * contributeur : William Allen Simpson <bill@cohortfs.com>
+ *		  Marcus Watts <mdw@cohortfs.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/**
+ * @file   internal.c
+ * @author Adam C. Emerson <aemerson@linuxbox.com>
+ * @author William Allen Simpson <bill@cohortfs.com>
+ * @author Marcus Watts <mdw@cohortfs.com>
+ * @date Wed Oct 22 13:24:33 2014
+ *
+ * @brief Internal definitions for the Cohort FSAL
+ *
+ * This file includes internal function definitions, constants, and
+ * variable declarations used to impelment the Cohort FSAL, but not
+ * exposed as part of the API.
+ */
+
+#include <sys/stat.h>
+#include "fsal_types.h"
+#include "fsal.h"
+#include "fsal_convert.h"
+#include "FSAL/fsal_commonlib.h"
+
+#define COHORT_INTERNAL_C
+#include "internal.h"
+
+/**
+ * The attributes tis FSAL can interpret or supply.
+ */
+
+const attrmask_t supported_attributes = (
+	ATTR_TYPE      | ATTR_SIZE     | ATTR_FSID  | ATTR_FILEID |
+	ATTR_MODE      | ATTR_NUMLINKS | ATTR_OWNER | ATTR_GROUP  |
+	ATTR_ATIME     | ATTR_RAWDEV   | ATTR_CTIME | ATTR_MTIME  |
+	ATTR_SPACEUSED | ATTR_CHGTIME);
+
+/**
+ * The attributes this FSAL can set.
+ */
+
+const attrmask_t settable_attributes = (
+	ATTR_MODE  | ATTR_OWNER | ATTR_GROUP | ATTR_ATIME	 |
+	ATTR_CTIME | ATTR_MTIME | ATTR_SIZE  | ATTR_MTIME_SERVER |
+	ATTR_ATIME_SERVER);
+
+/**
+ * @brief Convert a struct stat from Cohort to a struct attrlist
+ *
+ * This function writes the content of the supplied struct stat to the
+ * struct fsalsattr.
+ *
+ * @param[in]  buffstat Stat structure
+ * @param[out] fsalattr FSAL attributes
+ */
+
+void cohort2fsal_attributes(const struct stat *buffstat,
+			    struct attrlist *fsalattr)
+{
+	FSAL_CLEAR_MASK(fsalattr->mask);
+
+	/* Fills the output struct */
+	fsalattr->type = posix2fsal_type(buffstat->st_mode);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_TYPE);
+
+	fsalattr->filesize = buffstat->st_size;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_SIZE);
+
+	fsalattr->fsid = posix2fsal_fsid(buffstat->st_dev);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_FSID);
+
+	fsalattr->fileid = buffstat->st_ino;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_FILEID);
+
+	fsalattr->mode = unix2fsal_mode(buffstat->st_mode);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_MODE);
+
+	fsalattr->numlinks = buffstat->st_nlink;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_NUMLINKS);
+
+	fsalattr->owner = buffstat->st_uid;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_OWNER);
+
+	fsalattr->group = buffstat->st_gid;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_GROUP);
+
+	fsalattr->atime = posix2fsal_time(buffstat->st_atime, 0);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_ATIME);
+
+	fsalattr->ctime = posix2fsal_time(buffstat->st_ctime, 0);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_CTIME);
+
+	fsalattr->mtime = posix2fsal_time(buffstat->st_mtime, 0);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_MTIME);
+
+	fsalattr->chgtime =
+	    posix2fsal_time(MAX(buffstat->st_mtime, buffstat->st_ctime), 0);
+	fsalattr->change = fsalattr->chgtime.tv_sec;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_CHGTIME);
+
+	fsalattr->spaceused = buffstat->st_blocks * S_BLKSIZE;
+	FSAL_SET_MASK(fsalattr->mask, ATTR_SPACEUSED);
+
+	fsalattr->rawdev = posix2fsal_devt(buffstat->st_rdev);
+	FSAL_SET_MASK(fsalattr->mask, ATTR_RAWDEV);
+}
+
+/**
+ * @brief Construct a new filehandle
+ *
+ * This function constructs a new Cohort FSAL object handle and attaches
+ * it to the export.  After this call the attributes have been filled
+ * in and the handdle is up-to-date and usable.
+ *
+ * @param[in]  st     Stat data for the file
+ * @param[in]  export Export on which the object lives
+ * @param[out] obj    Object created
+ *
+ * @return 0 on success, negative error codes on failure.
+ */
+
+int construct_handle(const struct stat *st, struct Inode *i,
+		     struct cohort_export *export, struct cohort_handle **obj)
+{
+	/* Pointer to the handle under construction */
+	struct cohort_handle *constructing = NULL;
+
+	assert(i);
+	*obj = NULL;
+
+	constructing = gsh_calloc(1, sizeof(struct cohort_handle));
+	if (constructing == NULL)
+		return -ENOMEM;
+
+	constructing->vi.ino.val = st->st_ino;
+// XXX	constructing->vi.snapid.val = st->st_dev;
+	constructing->i = i;
+	constructing->up_ops = export->export.up_ops;
+
+	cohort2fsal_attributes(st, &constructing->handle.attributes);
+
+	fsal_obj_handle_init(&constructing->handle, &export->export,
+			     constructing->handle.attributes.type);
+
+	constructing->export = export;
+
+	*obj = constructing;
+
+	return 0;
+}
+
+/**
+ * @brief Release all resrouces for a handle
+ *
+ * @param[in] obj Handle to release
+ */
+
+void deconstruct_handle(struct cohort_handle *obj)
+{
+	ceph_ll_put(obj->export->cmount, obj->i);
+	fsal_obj_handle_uninit(&obj->handle);
+	gsh_free(obj);
+}
---
+++ src/FSAL/FSAL_COHORT/internal.h
@@ -0,0 +1,162 @@
+/*
+ * Copyright © 2012-2014, CohortFS, LLC.
+ * Author: Adam C. Emerson <aemerson@linuxbox.com>
+ *
+ * contributeur : William Allen Simpson <bill@cohortfs.com>
+ *		  Marcus Watts <mdw@cohortfs.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+/**
+ * @file   internal.h
+ * @author Adam C. Emerson <aemerson@linuxbox.com>
+ * @author William Allen Simpson <bill@cohortfs.com>
+ * @author Marcus Watts <mdw@cohortfs.com>
+ * @date Wed Oct 22 13:24:33 2014
+ *
+ * @brief Internal declarations for the Cohort FSAL
+ *
+ * This file includes declarations of data types, functions,
+ * variables, and constants for the Cohort FSAL.
+ */
+
+#ifndef FSAL_COHORT_INTERNAL_INTERNAL__
+#define FSAL_COHORT_INTERNAL_INTERNAL__
+
+#include <cephfs/libcephfs.h>
+#include <ceph_osd.h>
+#include "fsal.h"
+#include "fsal_types.h"
+#include "fsal_api.h"
+#include "fsal_convert.h"
+#include <stdbool.h>
+#include <uuid/uuid.h>
+
+/**
+ * Cohort Main (global) module object
+ */
+
+struct cohort_fsal_module {
+	struct fsal_module fsal;
+	struct libosd *osd;
+	char *where;
+};
+extern struct cohort_fsal_module CohortFSM;
+
+/**
+ * Cohort private export object
+ */
+
+struct cohort_export {
+	struct fsal_export export;	/*< The public export object */
+	struct ceph_mount_info *cmount;	/*< The mount object used to
+					   access all Cohort methods on
+					   this export. */
+	struct cohort_handle *root;	/*< The root handle */
+};
+
+/**
+ * The 'private' Cohort FSAL handle
+ */
+
+struct cohort_handle {
+	struct fsal_obj_handle handle;	/*< The public handle */
+	Fh *fd;
+	struct Inode *i;	/*< The Cohort inode */
+	const struct fsal_up_vector *up_ops;	/*< Upcall operations */
+	struct cohort_export *export;	/*< The first export listed */
+	vinodeno_t vi;		/*< The object identifier */
+	fsal_openflags_t openflags;
+#ifdef COHORT_PNFS
+	uint64_t rd_issued;
+	uint64_t rd_serial;
+	uint64_t rw_issued;
+	uint64_t rw_serial;
+	uint64_t rw_max_len;
+#endif				/* COHORT_PNFS */
+};
+
+/**
+ * The full, 'private' DS (data server) handle
+ */
+
+struct cohort_ds {
+	struct fsal_ds_handle ds;	/*< Public DS handle */
+	uuid_t volume;
+	char object_key[114];		/* MUST be same length as
+					struct alloc_file_handle_v4
+					pad[122] minus sizeof(uuid_t) */
+};
+
+#ifndef COHORT_INTERNAL_C
+/* Keep internal.c from clashing with itself */
+extern attrmask_t supported_attributes;
+extern attrmask_t settable_attributes;
+#endif				/* !COHORT_INTERNAL_C */
+
+/**
+ * Linux supports a stripe pattern with no more than 4096 stripes, but
+ * for now we stick to 1024 to keep them da_addrs from being too
+ * gigantic.
+ */
+
+static const size_t BIGGEST_PATTERN = 1024;
+
+/* private helper for export object */
+
+static inline fsal_staticfsinfo_t *cohort_staticinfo(struct fsal_module *hdl)
+{
+	struct cohort_fsal_module *myself =
+	    container_of(hdl, struct cohort_fsal_module, fsal);
+	return &myself->fs_info;
+}
+
+/* Prototypes */
+
+int construct_handle(const struct stat *st, struct Inode *i,
+		     struct cohort_export *export, struct cohort_handle **obj);
+void deconstruct_handle(struct cohort_handle *obj);
+
+/**
+ * @brief FSAL status from Cohort error
+ *
+ * This function returns a fsal_status_t with the FSAL error as the
+ * major, and the posix error as minor. (Cohort's error codes are just
+ * negative signed versions of POSIX error codes.)
+ *
+ * @param[in] cohort_errorcode Cohort error (negative Posix)
+ *
+ * @return FSAL status.
+ */
+static inline fsal_status_t cohort2fsal_error(const int cohort_errorcode)
+{
+	return fsalstat(posix2fsal_error(-cohort_errorcode), -cohort_errorcode);
+}
+void cohort2fsal_attributes(const struct stat *buffstat,
+			    struct attrlist *fsalattr);
+
+void export_ops_init(struct export_ops *ops);
+void handle_ops_init(struct fsal_obj_ops *ops);
+void ds_ops_init(struct fsal_ds_ops *ops);
+#ifdef COHORT_PNFS
+void export_ops_pnfs(struct export_ops *ops);
+void handle_ops_pnfs(struct fsal_obj_ops *ops);
+#endif				/* COHORT_PNFS */
+
+#endif				/* !FSAL_COHORT_INTERNAL_INTERNAL__ */
